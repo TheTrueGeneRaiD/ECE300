a0b5a03c0b3771da13e57a38260cf8ea6c01cfb8
Isaiah Mize <idcmize@gmail.com>
2022-02-26 21:39:42 -0700

    Modified and tested the dynamic matrix and used Doubles in the matrix then transposed it

--- Homework_3/test_dynamic.c
+++ Homework_3/test_dynamic.c
@@ -14,5 +14,31 @@ int main()
   matrix_print(b);
   printf("\n a+b:");
   matrix_print(add(a,b));
-}
 
+  printf("\n Matrix a:");
+  matrix aTransposed;
+  aTransposed = transpose(&a);
+  matrix_print(aTransposed);
+  printf("\n Matrix b:");
+  matrix_print(b);
+  printf("\n a+b:");
+  matrix_print(add(aTransposed,b));
+  
+  static T newData[] = {3, 2, 8, 7, 1, 1, 2, 4, 4, 1, 1, 2};
+  matrix z;
+  z = create_initvals(3, 4, newData);
+  matrix_print(z);
+  matrix zTransposed;
+  zTransposed = transpose(&z);
+  matrix_print(zTransposed);
+ 
+  static T doubleData[] = {3.2, 3.4, 8.1, 9.9, 1.0, 2.1, 2.11, 7.43};
+  matrix y;
+  y = create_initvals(2, 4, doubleData);
+  matrix_print(y);
+  matrix yTransposed;
+  yTransposed = transpose(&y);
+  matrix_print(yTransposed);
+
+  return 0;
+}
